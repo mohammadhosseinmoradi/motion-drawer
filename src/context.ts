d21d477ac45b51da64dfea27d8c45e79a5d55113
Mohammad Hossein Moradi <mhmdev64@gmail.com>
2025-09-20 22:14:01 +0330

    Simplify `DrawerContext`

--- src/context.ts
+++ src/context.ts
@@ -1,23 +1,12 @@
 import { createContext, RefObject, use } from "react";
-import { match } from "@/utils/match";
 
-type DrawerState = {};
-
-type DrawerActions = {};
-
-export const DrawerContext = createContext<
-  | ({
-      // These are part of the context but not part of the state to be dispatched
-      drawerRef: RefObject<HTMLElement | null>;
-      headerRef: RefObject<HTMLElement | null>;
-      bodyRef: RefObject<HTMLElement | null>;
-      actionsRef: RefObject<HTMLElement | null>;
-      maxSize: number;
-      isDrawerMaxSize: boolean;
-    } & DrawerState &
-      DrawerActions)
-  | null
->(null);
+export const DrawerContext = createContext<{
+  drawerRef: RefObject<HTMLElement | null>;
+  headerRef: RefObject<HTMLElement | null>;
+  bodyRef: RefObject<HTMLElement | null>;
+  actionsRef: RefObject<HTMLElement | null>;
+  maxSize: number;
+} | null>(null);
 
 export function useDrawerContext() {
   const context = use(DrawerContext);
@@ -32,26 +21,3 @@ export function useDrawerContext() {
   }
   return context;
 }
-
-export enum ActionTypes {
-  test,
-}
-
-type Actions = {
-  type: ActionTypes.test;
-};
-
-const reducers: {
-  [P in ActionTypes]: (
-    state: DrawerState,
-    action: Extract<Actions, { type: P }>,
-  ) => DrawerState;
-} = {
-  [ActionTypes.test](state) {
-    return state;
-  },
-};
-
-export const stateReducer = (state: DrawerState, action: Actions) => {
-  return match(action.type, reducers, state, action);
-};
