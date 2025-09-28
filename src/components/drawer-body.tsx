18fc1f1fb88c60e761ceb57e34695d953ba76dec
Mohammad Hossein Moradi <mhmdev64@gmail.com>
2025-09-28 23:10:50 +0330

    Implement spring scrolling

--- src/components/drawer-body.tsx
+++ src/components/drawer-body.tsx
@@ -1,13 +1,21 @@
 import { Props } from "@/utils/render/types";
-import { ElementType, Ref, useEffect, useMemo, useRef } from "react";
+import { ElementType, Ref, useCallback, useMemo, useRef } from "react";
 import { useRender } from "@/utils/render";
 import { DrawerRenderPropArg } from "@/types";
 import { useDrawerContext } from "@/context";
 import { syncRefs } from "@/utils/sync-refs";
 import { useDrag } from "@/hooks/use-drag";
-import { animate, useMotionValue, useMotionValueEvent } from "motion/react";
+import {
+  animate,
+  AnimationPlaybackControlsWithThen,
+  useMotionValue,
+  useMotionValueEvent,
+} from "motion/react";
 import { VELOCITY_MULTIPLIER } from "@/constants";
 import { clamp } from "@/utils/clamp";
+import { set } from "@/utils/set";
+import { rubberbandIfOutOfBounds } from "@/utils/rubberband-if-out-of-bounds";
+import { getMaxScrollTop } from "@/utils/scroll";
 
 const DEFAULT_DRAWER_BODY_TAG = "div";
 
@@ -32,37 +40,45 @@ export function DrawerBody<
     isScrolled: false,
     allowScroll: false,
   });
+  const releaseAnimationControl =
+    useRef<AnimationPlaybackControlsWithThen | null>(null);
+
+  const getIsDrawerMaxSize = useCallback(
+    (drawer: HTMLElement, maxSize: number) => {
+      return drawer.offsetHeight >= maxSize - 10;
+    },
+    [],
+  );
 
   const motionScrollTop = useMotionValue(bodyRef.current?.scrollTop || 0);
 
   const drag = useDrag({
     onInit() {
+      if (releaseAnimationControl.current)
+        releaseAnimationControl.current.stop();
       tracked.current.isScrolled = false;
       tracked.current.initialScrollTop = bodyRef.current!.scrollTop;
-      tracked.current.allowScroll = drawerRef.current!.offsetHeight >= maxSize;
+      tracked.current.allowScroll = getIsDrawerMaxSize(
+        drawerRef.current!,
+        maxSize,
+      );
     },
 
     onMove({ event, movement }) {
       if (!tracked.current.allowScroll) return;
       if (tracked.current.initialScrollTop === null) return;
 
-      const scrollTop = tracked.current.initialScrollTop + -movement[1];
-      const maxScrollTop =
-        bodyRef.current!.scrollHeight - bodyRef.current!.clientHeight;
+      const maxScrollTop = getMaxScrollTop(bodyRef.current!);
+      let scrollTop = tracked.current.initialScrollTop + -movement[1];
+      scrollTop = rubberbandIfOutOfBounds(scrollTop, 0, maxScrollTop);
 
       function scroll() {
         tracked.current.isScrolled = true;
         event.stopPropagation();
-        if (scrollTop <= maxScrollTop + 40 && scrollTop >= -40) {
-          motionScrollTop.set(scrollTop);
-        } else if (scrollTop > 0) {
-          motionScrollTop.set(maxScrollTop);
-        } else {
-          motionScrollTop.set(0);
-        }
+        motionScrollTop.set(scrollTop);
       }
 
-      const isDrawerMaxSize = drawerRef.current!.offsetHeight >= maxSize;
+      const isDrawerMaxSize = getIsDrawerMaxSize(drawerRef.current!, maxSize);
 
       // Scroll down
       if (
@@ -87,30 +103,56 @@ export function DrawerBody<
     onRelease({ velocity, direction }) {
       if (!tracked.current.allowScroll) return;
       if (!tracked.current.isScrolled) return;
+      if (!bodyRef.current) return;
 
-      const maxScrollTop =
-        bodyRef.current!.scrollHeight - bodyRef.current!.clientHeight;
-      const fromSize = bodyRef.current!.scrollTop;
-      let toSize = fromSize + velocity[1] * VELOCITY_MULTIPLIER * -direction[1];
-      toSize = clamp(toSize, 0, maxScrollTop);
+      const maxScrollTop = getMaxScrollTop(bodyRef.current);
+      const fromSize = motionScrollTop.get();
+      const toSize =
+        fromSize + velocity[1] * VELOCITY_MULTIPLIER * 2 * -direction[1];
+      const toSizeClamped = clamp(toSize, 0, maxScrollTop);
 
-      animate(fromSize, toSize, {
-        damping: 10,
-        stiffness: 100,
-        mass: 1,
+      // If toSize is out of bounds, use a greater mass
+      const mass =
+        (toSize < 0 && fromSize >= 0) ||
+        (toSize > maxScrollTop && fromSize <= maxScrollTop)
+          ? 4
+          : 1;
+
+      releaseAnimationControl.current = animate(fromSize, toSizeClamped, {
+        type: "spring",
+        damping: 100,
+        stiffness: clamp(450 * clamp(velocity[1], 1, 20), 100, 1000),
+        mass: mass,
         onUpdate: (latest) => {
-          bodyRef.current!.scrollTop = latest;
+          motionScrollTop.set(latest);
         },
       });
     },
   });
 
-  useEffect(() => {
-    motionScrollTop.set(bodyRef.current!.scrollTop);
-  }, []);
-
   useMotionValueEvent(motionScrollTop, "change", (scrollTop) => {
-    bodyRef.current!.scrollTop = scrollTop;
+    if (!bodyRef.current) return;
+
+    const maxScrollTop = getMaxScrollTop(bodyRef.current);
+
+    let overflow = 0;
+    let transformOrigin = "unset";
+    if (scrollTop < 0) {
+      overflow = Math.abs(scrollTop);
+      transformOrigin = "top";
+    } else if (scrollTop > maxScrollTop) {
+      overflow = scrollTop - maxScrollTop;
+      transformOrigin = "bottom";
+    }
+
+    const bodyChild = bodyRef.current!.children[0] as HTMLElement;
+
+    set(bodyChild, {
+      "transform-origin": transformOrigin,
+      transform: `scaleY(${1 + overflow * 0.001})`,
+    });
+
+    bodyRef.current!.scrollTop = clamp(scrollTop, 0, maxScrollTop);
   });
 
   const slot = useMemo(() => {
